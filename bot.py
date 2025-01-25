af718fbe2f2429ab6637e2168a69c429e71a4e3e
Robin <robin.carl@fau.de>
2025-01-25 15:21:43 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import telebot
+import google.generativeai as genai
+
+with open("token.txt", "r") as f:
+    BOT_TOKEN = f.read()
+with open("gemini_token.txt", "r") as f:
+    GEMINI_TOKEN = f.read()
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+genai.configure(api_key=GEMINI_TOKEN)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    response = model.generate_content(message.text)
+    bot.reply_to(message, response.text)
+
+def main():
+    bot.infinity_polling()
+
+if __name__ == "__main__":
+    main()
