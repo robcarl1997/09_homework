3a6ba4a735dc7c86a080c7d5fb81c2d6a2d01a55
Robin <robin.carl@fau.de>
2025-01-25 15:51:23 +0100

    adjustions

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import telebot
-import google.generativeai as genai
 
 with open("token.txt", "r") as f:
     BOT_TOKEN = f.read()
-with open("gemini_token.txt", "r") as f:
-    GEMINI_TOKEN = f.read()
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+import google.generativeai as genai
+
+with open("gemini_token.txt", "r") as f:
+    GEMINI_TOKEN = f.read()
 
 genai.configure(api_key=GEMINI_TOKEN)
 model = genai.GenerativeModel("gemini-1.5-flash")
